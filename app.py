8b824d756d5274347a876dc3763179e6df16acb6
Aleks Tapinsh <aleks@tapinsh.com>
2016-03-06 08:35:07 -0500

    adds json calls

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mysqldb import MySQL
+import urllib
+import string
+import simplejson as json
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,15 +20,17 @@ app.config.from_pyfile('config.py')
 manager = Manager(app)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 
 
 #routes
 @app.route('/')
 def home():
-    db.create_all()
-    db.drop_all()
+    wiki_data = get_wiki()
+    for key, value in wiki_data.iteritems():
+        title = clean_title(value['title'])
+        print title
+    giffy_data = get_giffy(title)
+    print giffy_data
     return render_template('index.html')
 
 #database models
@@ -40,6 +45,39 @@ class Image(db.Model):
     
     def __repr__(self):
         return '<Image %r>' % self.url
+        
+#helper methods
+#gets a json object from a URL
+def get_json(URL):
+    fileObj = urllib.urlopen(URL)
+    parsed_json = json.load(fileObj)
+    return parsed_json
+
+#gets a random Wikipedia page
+def get_wiki():
+    url = app.config['WIKI_URL']
+    json = get_json(url)
+    data = json['query']['pages']
+    return data
+
+def clean_title(title):
+    title = title.replace("-", " ")
+    printable = set(string.printable)
+    title = filter(lambda x: x in printable, title) #strip non-ASCII characters
+    return title
+
+#gets a giffy URL from API
+def get_giffy(title):
+     data = {}
+     url = app.config['GIFFY_URL']
+     title = title.replace(" ", "+")
+     url = url + title
+     json = get_json(url)
+     raw_data = dict(json['data'])
+     if raw_data:
+         data['image_url'] = raw_data['fixed_width_downsampled_url'].encode('ascii')
+     return data
+         
 
 if __name__ == '__main__':
     manager.run()
